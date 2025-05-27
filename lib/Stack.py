8c8f8e0dd50fbfd8c39f558fa1e141ee96fba471
Sitong <nickren93@gmail.com>
2025-05-26 21:30:08 -0400

    complete lab

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,52 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        #pass
+        self.items = []
+        self.limit = limit
+        for element in items:
+            self.items.append(element)
 
     def isEmpty(self):
-        pass
+        #pass
+        if len(self.items) == 0:
+            return True
+        else:
+            return False
 
     def push(self, item):
-        pass
+        #pass
+        if len(self.items) < self.limit:
+            self.items.append(item)
+        else:
+            return "Stack overflow"
 
     def pop(self):
-        pass
+        #pass
+        if len(self.items) > 0:
+            return self.items.pop()
 
     def peek(self):
-        pass
+        #pass
+        return self.items[-1]
     
     def size(self):
-        pass
+        #pass
+        return len(self.items)
 
     def full(self):
-        pass
+        #pass
+        if len(self.items) == self.limit:
+            return True
+        else:
+            return False
 
     def search(self, target):
-        pass
+        #pass
+        if target in self.items:
+            if self.items.index(target) == (self.size()-1):
+                return 0
+            else:
+                return (self.size()-1) - self.items.index(target)
+        else:
+            return -1
